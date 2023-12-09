d46d8037bbb7fc6c8ff298b9114cd2183053b570
lennartkaden <dev@lenniemail.de>
2023-12-09 16:40:19 +0100

    :memo: Update API description in FastAPI initialization
    Changed the description text of the FastAPI object initialization in main.py. The university name in the API description has been generalized to enhance the project's adaptability and to avoid specific naming.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi_versioning import VersionedFastAPI
 
 from versions.v1.user_routes import router as v1_router
 
-app = FastAPI(title="QISsy", description="A simple API to access the QIS server of the Leibniz University Hannover")
+app = FastAPI(title="QISsy", description="A simple API to access the QIS server of a University")
 
 app.include_router(v1_router)
 
